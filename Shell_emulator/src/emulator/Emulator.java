131538e79a6ccbf9b08c8205f4e4b157fed677e6
Melee Fox <fox.melee@yandex.ru>
2024-10-04 16:50:05 +0300

    Создание примерной архитектуры программы

---
+++ Shell_emulator/src/emulator/Emulator.java
@@ -0,0 +1,32 @@
+package emulator;
+
+public class Emulator {
+    //конструктор
+    Emulator(){
+
+    }
+    //реализация комманды ls
+    public void ls_Command(){
+        
+    }
+    //реализация комманды cd
+    public void cd_Command(){
+
+    }
+    //реализация комманды exit
+    public void exit_Command(){
+
+    }
+    //реализация комманды cat
+    public void cat_Command(){
+
+    }
+    //реализация комманды rev
+    public void rev_Command(){
+
+    }
+    //реализация обработки комманд
+    public void command_Reader(String command){
+        
+    }
+}
